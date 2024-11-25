88616be08186cdfa0e1c722920233ed29fb84cea
byambadorj2 <byambadorj993@gmail.com>
2024-11-25 18:27:05 +0800

    upload song page

---
+++ lib/features/home/view/pages/upload_song_page.dart
@@ -0,0 +1,35 @@
+import 'package:dotted_border/dotted_border.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class UploadSongPage extends ConsumerStatefulWidget {
+  const UploadSongPage({super.key});
+
+  @override
+  ConsumerState<UploadSongPage> createState() => _UploadSongPageState();
+}
+
+class _UploadSongPageState extends ConsumerState<UploadSongPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Upload Song"),
+      ),
+      body: Column(
+        children: [
+          DottedBorder(
+              child: Column(
+            children: [
+              Icon(
+                Icons.folder_open,
+                size: 40,
+              ),
+              Text("Select the thumbnail"),
+            ],
+          ))
+        ],
+      ),
+    );
+  }
+}
